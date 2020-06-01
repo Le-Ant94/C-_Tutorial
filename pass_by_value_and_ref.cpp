29736a4cd55e926095c7b117b130fb186c5ba96b
Anthony Dao <37991277+Le-Ant94@users.noreply.github.com>
2020-05-31 21:30:37 -0500

    Add files via upload

--- pass_by_value_and_ref.cpp
+++ pass_by_value_and_ref.cpp
@@ -10,12 +10,25 @@ void swap(int a, int b) //try &a and &b and see the difference
   std::cout << "a: " << a << "\tb: " << b << "\n";
 }
 
+void swap(std::string a, std::string b) //function overloading; same variables a & b but are strings this time
+{
+  std::string temp = a;
+  a = b;
+  b = temp;
+}
+
 int main()
 {
   int a = 10;
   int b = 20;
 
-  swap(a, b);
+  swap(a, b); //displaying results according to function void swap(int a, int b)
 
-  std::cout << "a: " << a << "\tb: " << b << "\n";
+  std::string first_name = "Anthony";
+  std::string last_name = "Dao";
+
+  swap(first_name, last_name); //displaying results according to function void swap(std::string a, std::string b)
+  std::cout << first_name << " " << last_name << std::endl;
+
+  std::cout << "a: " << a << "\tb: " << b << "\n"; //displaying results for the declared values of a and b
 }
